ac8429a2cb75333f626cb1bebd510741ea5c49f8
Edgar-Moises-L <moisesleyva202@gmail.com>
2025-05-05 16:32:37 -0700

    feat: creacion de pantalla de ventas

---
+++ src/main/java/Venta.java
@@ -0,0 +1,32 @@
+import com.formdev.flatlaf.FlatDarculaLaf;
+
+import javax.swing.*;
+
+public class Venta extends JFrame{
+
+    public Venta (){
+        iniciarVenta();
+    }
+
+    private void iniciarVenta(){
+        setContentPane(panelPrincipal);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(900,700);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+        FlatDarculaLaf.setup();
+        Venta venta = new Venta();
+        venta.setVisible(true);
+    }
+
+    private JPanel panelPrincipal;
+    private JTable table1;
+    private JButton cobrarButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton agregarButton;
+    private JButton eliminarButton;
+}
